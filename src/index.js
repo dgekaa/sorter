fd08a33d6a5312e73a6f121f004ef892916e896b
evgeniy <dgeka_vip@mail.ru>
2018-09-18 07:40:44 +0300

    sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.x = []; 
   }
 
   add(element) {
-    // your implementation
+    this.x.push(element);
+    return this.x; 
   }
 
   at(index) {
-    // your implementation
+    return this.x[index]; 
   }
 
   get length() {
-    // your implementation
+    return this.x.length; 
   }
 
   toArray() {
-    // your implementation
+    return this.x; 
   }
 
-  sort(indices) {
-    // your implementation
+  sort(indices=[]) {
+    indices.sort(function(a,b) {return a-b});
+
+    var e = this.x.slice(indices[0],indices[indices.length-1]);
+    e.push(this.x[indices[indices.length-1]]);
+
+    e.sort(function(a,b) {return a-b});
+
+    for(var j = 0; j<indices.length; j++){
+    this.x.splice(indices[j],1,e[j]);
+    }
+    return this.x; 
   }
 
   setComparator(compareFunction) {
-    // your implementation
+   Sorter.prototype.sort = function(indices=[]) {
+          indices.sort(function(a,b) {return a-b});
+          var e = this.x.slice(indices[0],indices[indices.length-1]);
+          e.push(this.x[indices[indices.length-1]]);
+          e.sort(function(a,b) {return b-a});
+          for(var j = 0; j<indices.length; j++){
+          this.x.splice(indices[j],1,e[j]);
+          }
+          return this.x; 
+    }
   }
 }
 
